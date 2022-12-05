9b263ec4a95780370cd172fe6d3da63fe14fec04
Anthony Defraine <anthony.defraine@ekwateur.fr>
2022-12-05 08:57:33 +0100

    Testing MeasurementReader

--- src/main/java/abb/interview/reader/MeasurementReader.java
+++ src/main/java/abb/interview/reader/MeasurementReader.java
@@ -1,8 +1,13 @@
 package abb.interview.reader;
 
 import abb.interview.domain.Measurement;
+import abb.interview.domain.Measurements;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class MeasurementReader {
@@ -10,8 +15,19 @@ public class MeasurementReader {
     private MeasurementReader() {
     }
 
-    public static List<Measurement> readValue(final File file) {
-        return List.of();
+    public static List<Measurement> readValue(final File file) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+
+        return objectMapper.readValue(file, new TypeReference<List<Measurement>>() {});
+    }
+
+    // What to do in case of duplicate ?
+    public static void groupByKey(final Measurements measurements, List<Measurement> values) {
+        for (Measurement value : values) {
+            measurements.put(value.getKey(), value);
+        }
     }
 
 
--- src/test/java/abb/interview/reader/MeasurementReaderTest.java
+++ src/test/java/abb/interview/reader/MeasurementReaderTest.java
@@ -1,25 +1,59 @@
 package abb.interview.reader;
 
 import abb.interview.domain.Measurement;
+import abb.interview.domain.MeasurementSample;
+import abb.interview.domain.Power;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MeasurementReaderTest {
 
     @Test
-    void readFileShouldMatchTheMeasurementObjectStructure() {
+    void readFileShouldMatchTheMeasurementObjectStructure() throws IOException {
         // read file into list
         List<Measurement> result = MeasurementReader.readValue(new File("src/test/resources/measurements.json"));
 
+        // null is not acceptable
         Assertions.assertNotNull(result);
-        Assertions.assertNotEquals(0, result.size());
 
-        // assert measurement structure is correct
+        // Size is a known value, remove magic number and use proper count when we produce the file
+        Assertions.assertEquals(40, result.size());
 
-        // assert power structure is correct
+        // Testing only 1 object, we should produce a dynamic file and test all objects.
+        // We are only testing that the Object Measurement and the file measurements.json have the same data structure
+        Measurement expectedMeasurement = MeasurementSample.firstItem();
+        Measurement actualMeasurement = result.iterator().next();
+
+        // We could implement Equals and Hashcode in Measurement, but it could be a bad idea if used in a Set
+        assertAll("measurement",
+                () -> assertEquals(expectedMeasurement.getResourceId(), actualMeasurement.getResourceId()),
+                () -> assertEquals(expectedMeasurement.getDeviceName(), actualMeasurement.getDeviceName()),
+                () -> assertEquals(expectedMeasurement.getDeviceGroup(), actualMeasurement.getDeviceGroup()),
+                () -> assertEquals(expectedMeasurement.getDirection(), actualMeasurement.getDirection())
+        );
+
+        List<Power> expectedPowers = expectedMeasurement.getPower();
+        List<Power> actualPowers = actualMeasurement.getPower();
+
+        for (int i = 0; i < actualPowers.size(); i++) {
+            Power expectedPower = expectedPowers.get(i);
+            Power actualPower = actualPowers.get(i);
+
+            // We could implement Equals and Hashcode in Power, but it could be a bad idea if used in a Set
+            assertAll("power",
+                    () -> assertEquals(expectedPower.getMin(), actualPower.getMin()),
+                    () -> assertEquals(expectedPower.getMax(), actualPower.getMax()),
+                    () -> assertEquals(expectedPower.getAvg(), actualPower.getAvg()),
+                    () -> assertEquals(expectedPower.getTimestamp(), actualPower.getTimestamp())
+            );
+        }
     }
 
 }
